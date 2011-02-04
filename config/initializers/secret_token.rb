d446c9133b733b83b1f6117fbce499ff79c43307
Esteban Munoz <emunoz@host-111.sfoffice.verticalresponse.com>
2011-02-03 22:54:55 -0800

    Initial Rails app structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AvanticaGreen::Application.config.secret_token = '612fe9acca4994e79fcc5203fb3ac76a20ba110e7186541d568effa4a2e203d2cd7ddb18445df2b8925bd8b0103c6e69d7b0a022d544a10d75edb0a66034a184'
