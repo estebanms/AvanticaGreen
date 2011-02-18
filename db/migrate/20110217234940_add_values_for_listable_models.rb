b053b8fee5c91ab74241548208f55ab6593a583d
eincer <eduangutan@hotmail.com>
2011-02-17 21:33:09 -0800

    agregando mas datos a la migracion que genera datos de prueba

--- db/migrate/20110217234940_add_values_for_listable_models.rb
+++ db/migrate/20110217234940_add_values_for_listable_models.rb
@@ -21,12 +21,31 @@ class AddValuesForListableModels < ActiveRecord::Migration
     { :name => 'Rejected', :description => 'Rejected' },
     { :name => 'Accepted', :description => 'Accepted' },
   ]
+  @@games = [
+    { :name => 'old game', :start_date => '2010-11-01', :end_date => '2010-12-31', :active => false },
+    { :name => 'current game', :start_date => '2011-01-01', :end_date => '2011-06-31', :active => true },
+  ]
+  @@teams = [
+    { :name => 'cerditos', :description => 'los cerditos del 5 piso', :code => 'xyzu'},
+    { :name => 'cucarachas', :description => 'los cucas del piso numero 2', :code => 'poiu'},
+    { :name => 'artesanos', :description => 'los camaradas del 6 piso', :code => 'tlkb'},
+    { :name => 'barracudas', :description => 'los artistas del 4 piso', :code => 'qwer'},
+  ]
+  @@players = [
+    { :name => 'amanda', :last_names => 'segovia',:user_id => 1, :team_id => 1},
+  ]
+  
+  
   
   def self.up
     @@comment_types.each { |type| CommentType.new(:name => type[:name], :description => type[:description]).save }
     @@suggestion_types.each { |type| SuggestionType.new(:name => type[:name], :description => type[:description]).save }
     @@infraction_types.each { |type| InfractionType.new(:name => type[:name], :description => type[:description]).save }
     @@statuses.each { |type| Status.new(:name => type[:name], :description => type[:description]).save }
+    @@games.each { |type| Game.new(:name => type[:name], :active => type[:active], :start_date => type[:start_date], :end_date => type[:end_date]).save }
+    @@teams.each { |type| Team.new(:name => type[:name], :description => type[:description], :code => type[:code]).save              
+    }
+    @@players.each { |type| Player.new(:name => type[:name], :last_names => type[:last_names],:user_id => type[:user_id], :team_id => type[:team_id]).save }
   end
 
   def self.down
@@ -34,5 +53,8 @@ class AddValuesForListableModels < ActiveRecord::Migration
     @@suggestion_types.each { |type| SuggestionType.delete_all(:name => type[:name]) }
     @@infraction_types.each { |type| InfractionType.delete_all(:name => type[:name]) }
     @@statuses.each { |type| Status.delete_all(:name => type[:name]) }
+    @@games.each { |type| Game.delete_all(:name => type[:name]) }
+    @@teams.each { |type| Team.delete_all(:name => type[:name]) }
+    @@players.each { |type| Team.delete_all(:name => type[:name]) }
   end
 end
