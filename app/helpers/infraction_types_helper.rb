2ebf60beaa35c2ebe16dbf351990795b77cddf4e
eincer <eduangutan@hotmail.com>
2011-04-02 13:09:19 -0600

    agregando active para Player, Team e Infraction Type. Inactivos son listados para usuarios administradores unicamente en listaods, no en dropdowns

--- app/helpers/infraction_types_helper.rb
+++ app/helpers/infraction_types_helper.rb
@@ -1,5 +1,5 @@
 module InfractionTypesHelper
   def infraction_types_select(object = :infraction, field_name = :infraction_type_id, options = {})
-    collection_select(object, field_name, InfractionType.all, :id, :name, options)
+    collection_select(object, field_name, InfractionType.where(:active => true), :id, :name, options)
   end
 end
--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,5 +1,5 @@
 module TeamsHelper
   def teams_select(object, field_name = :team_id, options = {})
-    collection_select(object, field_name, Team.all, :id, :name, options)
+    collection_select(object, field_name, Team.where(:active => true), :id, :name, options)
   end
 end
