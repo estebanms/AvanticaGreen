b774f9e73479ac91390ece3be1ee8579121e602b
eincer <eduangutan@hotmail.com>
2011-05-04 21:18:58 -0600

    arreglando pulga de is_admin (al hacer click en players y no estar logueado)

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,7 +5,7 @@ class PlayersController < ApplicationController
   # GET /players.xml
   def index
     
-    @players = Player.where(:active => true) unless current_player.is_admin?
+    @players = Player.where(:active => true) unless current_player && current_player.is_admin?
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @players }
