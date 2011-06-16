c572c46f9e13d32ba8eb3e7c470785264ae30c94
Esteban Munoz <emunoz@host-111.sfoffice.verticalresponse.com>
2011-06-15 17:19:31 -0700

    Arreglando issue #20

--- app/models/witness.rb
+++ app/models/witness.rb
@@ -1,9 +1,15 @@
+class DifferentTeamValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    record.errors[attribute] << 'cannot be part of the reporter team' if record.infraction.team == record.player.team
+  end
+end
+
 class Witness < ActiveRecord::Base
   belongs_to :player
   belongs_to :infraction
   belongs_to :status
   
-  validates :player, :presence => true
+  validates :player, :presence => true, :different_team => true
   validates :infraction, :presence => true
   
   before_destroy do
