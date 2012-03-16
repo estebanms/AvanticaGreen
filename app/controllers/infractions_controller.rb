c8ed9268db3393cd478998aad65c7b9bc720d5c9
Esteban Munoz <emunoz@host-111.sfoffice.verticalresponse.com>
2012-03-16 00:44:24 -0700

    Agregando logica en el controlador de infracciones para el filtro de busqueda para el issue #27

--- app/controllers/infractions_controller.rb
+++ app/controllers/infractions_controller.rb
@@ -6,8 +6,16 @@ class InfractionsController < ApplicationController
   # GET /infractions.xml
   def index
     # the list of infractions is within the scope of a game
-    # TODO: add filters to let the user see infractions from other games, dates, types (open only, ...), etc
     @infractions = current_game.infractions rescue Array.new
+    # filter the infractions if the user limited the search to certain conditions
+    if params[:infraction]
+      # do some validations on the filter fields
+      # 1. restrict search fields to only: team_id, offender_id, infraction_type and status_id
+      params[:infraction].keep_if do |field, value|
+        [:team_id, :offender_id, :infraction_type_id, :status_id].include?(field.to_sym) and !value.blank?
+      end
+      @infractions = @infractions.where(params[:infraction]) unless params[:infraction].empty?
+    end
 
     respond_to do |format|
       format.html # index.html.erb
