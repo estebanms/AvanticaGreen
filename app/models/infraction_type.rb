8d837b6b0ecc89e33244ef8f6852ddb570a2f8e5
Ricardo Redondo <ricardo.redondo@gmail.com>
2013-09-25 14:52:59 -0600

    Bug: Infraction points negative - System allowed negative values for the infraction points

--- app/models/infraction_type.rb
+++ app/models/infraction_type.rb
@@ -1,10 +1,10 @@
 class InfractionType < ActiveRecord::Base
-  attr_accessible :name, :description, :active
+  attr_accessible :points, :name, :description, :active
   # might be useful for statistics
   has_many :infractions, :dependent => :restrict_with_exception
   
   validates :name, :presence => true, :uniqueness => true
-  validates :points, :numericality => true, :allow_nil => true
+  validates :points, :numericality => { allow_nil: false, :only_integer => true, :greater_than_or_equal_to => 0 }
   
   def to_s
     self.name
