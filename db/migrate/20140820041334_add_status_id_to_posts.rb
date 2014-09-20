4d60cd5183804ed3ea88ebfb3f1f44d422f0ed58
edwin incer <eduangutan@hotmail.com>
2014-09-20 17:12:35 -0600

    Fxing migration order

--- db/migrate/20140820041334_add_status_id_to_posts.rb
+++ db/migrate/20140820041334_add_status_id_to_posts.rb
@@ -2,7 +2,7 @@ class AddStatusIdToPosts < ActiveRecord::Migration
   def self.up
     add_column :posts, :status_id, :integer
     # mark the existing suggestions as "Accepted"
-    Post.update_all("status_id = #{Status.accepted.id}", 'status_id IS NULL')
+    Post.where('status_id IS NULL').update_all("status_id = #{Status.accepted.id}")
     Status.create(:name => 'Closed', :description => 'Closed')
   end
 
